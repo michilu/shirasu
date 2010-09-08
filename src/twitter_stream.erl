4be8f9caa745ff000cd85daa4ad2ab15e7c878b2
MiCHiLU <djmchl@gmail.com>
2010-09-09 08:12:31 +0900

    Updated twitter_stream.erl

--- src/twitter_stream.erl
+++ src/twitter_stream.erl
@@ -1,5 +1,7 @@
 -module(twitter_stream).
 -author('jebu@jebu.net').
+-author('Takanao ENDOH <djmchl@gmail.com>').
+%% Origin: http://blog.jebu.net/2009/09/erlang-tap-to-the-twitter-stream/
 %% 
 %% Copyright (c) 2009, Jebu Ittiachen
 %% All rights reserved.
@@ -29,63 +31,69 @@
 %% or implied, of Jebu Ittiachen.
 %%
 %% API
--export([fetch/1, fetch/3, process_data/1]).
+-export([fetch/1, fetch/2, fetch/4, process_data/2]).
  
+fetch(URL) ->
+  fetch(URL, fun(Data) ->
+              error_logger:info_msg("Received tweet ~p ~n", [Data])
+             end).
+
 % single arg version expects url of the form http://user:password@stream.twitter.com/1/statuses/sample.json
 % this will spawn the 3 arg version so the shell is free
-fetch(URL) ->
-  spawn(twitter_stream, fetch, [URL, 5, 30]).
+fetch(URL, Callback) ->
+  spawn(?MODULE, fetch, [URL, Callback, 5, 30]).
  
 % 3 arg version expects url of the form http://user:password@stream.twitter.com/1/statuses/sample.json  
 % retry - number of times the stream is reconnected
 % sleep - secs to sleep between retries.
-fetch(URL, Retry, Sleep) when Retry > 0 ->
+fetch(URL, Callback, Retry, Sleep) when Retry > 0 ->
   % setup the request to process async
   % and have it stream the data back to this process
+  ok = inets:start(),
   try http:request(get, 
                     {URL, []},
                     [], 
                     [{sync, false}, 
                      {stream, self}]) of
     {ok, RequestId} ->
-      case receive_chunk(RequestId) of
+      case receive_chunk(RequestId, Callback) of
         {ok, _} ->
           % stream broke normally retry 
           timer:sleep(Sleep * 1000),
-          fetch(URL, Retry - 1, Sleep);
+          fetch(URL, Callback, Retry - 1, Sleep);
         {error, unauthorized, Result} ->
           {error, Result, unauthorized};
         {error, timeout} ->
           timer:sleep(Sleep * 1000),
-          fetch(URL, Retry - 1, Sleep);
+          fetch(URL, Callback, Retry - 1, Sleep);
         {_, Reason} ->
           error_logger:info_msg("Got some Reason ~p ~n", [Reason]),
           timer:sleep(Sleep * 1000),
-          fetch(URL, Retry - 1, Sleep)
+          fetch(URL, Callback, Retry - 1, Sleep)
       end;
     _ ->
       timer:sleep(Sleep * 1000),
-      fetch(URL, Retry - 1, Sleep)
+      fetch(URL, Callback, Retry - 1, Sleep)
   catch 
     _:_ -> 
       timer:sleep(Sleep * 1000),
-      fetch(URL, Retry - 1, Sleep)
+      fetch(URL, Callback, Retry - 1, Sleep)
   end;
 %
-fetch(_, Retry, _) when Retry =< 0 ->
+fetch(_, _, Retry, _) when Retry =< 0 ->
   error_logger:info_msg("No more retries done with processing fetch thread~n"),
   {error, no_more_retry}.
 %
 % this is the tweet handler persumably you could do something useful here
 %
-process_data(Data) ->
-  error_logger:info_msg("Received tweet ~p ~n", [Data]),
+process_data(Fun, Data) ->
+  Fun(Data),
   ok.
  
 %%====================================================================
 %% Internal functions
 %%====================================================================
-receive_chunk(RequestId) ->
+receive_chunk(RequestId, Callback) ->
   receive
     {http, {RequestId, {error, Reason}}} when(Reason =:= etimedout) orelse(Reason =:= timeout) -> 
       {error, timeout};
@@ -97,14 +105,14 @@ receive_chunk(RequestId) ->
     %% start of streaming data
     {http,{RequestId, stream_start, Headers}} ->
       error_logger:info_msg("Streaming data start ~p ~n",[Headers]),
-      receive_chunk(RequestId);
+      receive_chunk(RequestId, Callback);
  
     %% streaming chunk of data
     %% this is where we will be looping around, 
     %% we spawn this off to a seperate process as soon as we get the chunk and go back to receiving the tweets
     {http,{RequestId, stream, Data}} ->
-      spawn(twitter_stream, process_data, [Data]),
-      receive_chunk(RequestId);
+      spawn(?MODULE, process_data, [Callback, Data]),
+      receive_chunk(RequestId, Callback);
  
     %% end of streaming data
     {http,{RequestId, stream_end, Headers}} ->
@@ -114,5 +122,4 @@ receive_chunk(RequestId) ->
   %% timeout
   after 60 * 1000 ->
     {error, timeout}
- 
   end.
