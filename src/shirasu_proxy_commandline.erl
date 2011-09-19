c57e4890c9ccf34bfef133ab800ff794cc1f7395
MiCHiLU <djmchl@gmail.com>
2011-09-19 10:34:36 +0900

    shirasu_proxy_commandline module was supported multi commands in config.

--- src/shirasu_proxy_commandline.erl
+++ src/shirasu_proxy_commandline.erl
@@ -1,6 +1,6 @@
 -module(shirasu_proxy_commandline).
 -author('ENDOH takanao <djmchl@gmail.com>').
--export([start/0, command_handler/2]).
+-export([start/0, commands/2]).
 
 -include_lib("eunit/include/eunit.hrl").
 %?debugVal(),
@@ -8,8 +8,19 @@
 start() ->
   CfgList = shirasu:cfg(["shirasu_proxy_commandline"]),
   [{Path, Cmd}|_T] = CfgList,
-  spawn(?MODULE, command_handler, [binary_to_list(Cmd), {binary_to_list(Path)}]),
+  Opt = {binary_to_list(Path)},
+  spawn(?MODULE, commands, [Cmd, Opt]),
   ok.
+
+commands(Cmd, Opt) when is_bitstring(Cmd) ->
+  command_handler(binary_to_list(Cmd), Opt),
+  ok;
+commands(Cmds, Opt) ->
+  lists:map(fun(Cmd) ->
+              command_handler(binary_to_list(Cmd), Opt)
+            end,
+            Cmds),
+  commands(Cmds, Opt).
 
 command_handler(Cmd, Opt) ->
   Port = open_port({spawn, Cmd}, [stream, exit_status]),
@@ -26,12 +37,12 @@ receive_command_response(Port, {Path}) ->
                       %?debugVal(io:format("~p", [{Path, Line}])),
                       wsManager ! {send, Path, Line}
                   end
-                end, string:tokens(Data, "\r\n"));
-    {Port, {exit_status, Status}} ->
-      ?debugVal(io:format("~p", [{Port, {exit_status, Status}}])),
+                end, string:tokens(Data, "\r\n")),
+      receive_command_response(Port, {Path});
+    {Port, {exit_status, _Status}} ->
       pass;
     Any ->
       ?debugVal(io:format("~p", [Any])),
       pass
   end,
-  receive_command_response(Port, {Path}).
+  ok.
