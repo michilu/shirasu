a6ccb89245221fcd6fb7471e3f1be14107d4a35e
MiCHiLU <djmchl@gmail.com>
2011-10-06 09:34:52 +0900

    Refactored. Moved wsManager to shirasu.erl.

--- src/shirasu.erl
+++ src/shirasu.erl
@@ -1,6 +1,6 @@
 -module(shirasu).
 -author('ENDOH takanao <djmchl@gmail.com>').
--export([start/0, stop/0, boot/0, cfg/1, cfgManager/1]).
+-export([start/0, stop/0, boot/0, cfg/1, cfgManager/1, handle_websocket/2, wsManager/0]).
 
 -include_lib("eunit/include/eunit.hrl").
 %?debugVal(),
@@ -20,7 +20,7 @@ boot() ->
     Modules = getModules(),
     ok = inets:start(),
     lists:map(fun(Module) -> register(Module, spawn(Module, start, [])) end, Modules),
-    spawn(shirasu_websocket, wsManager, []),
+    spawn(?MODULE, wsManager, []),
     ServerPid = listen_port(cfg(["shirasu"])),
     ServerPid.
 
@@ -31,7 +31,7 @@ listen_port([{struct, PropList}|_T]) ->
         {loop, fun(Req) ->
                     shirasu_http_serve:handle_http(Req) end},
         {ws_loop, fun(Ws) ->
-                    shirasu_websocket:handle_websocket(new, Ws) end},
+                    ?MODULE:handle_websocket(new, Ws) end},
         {ws_autoexit, false}
     ],
     case proplists:get_value(<<"ssl">>, PropList) of
@@ -118,3 +118,94 @@ getModules() ->
                            end,
                            Keys),
     Modules.
+
+handle_websocket(Ws) ->
+    receive
+            {browser, Data} ->
+                    %?debugVal({"ws:browser", Data}),
+                    wsManager ! {send, Ws:get(path), Data},
+                    handle_websocket(Ws);
+            closed ->
+                    wsManager ! {del, Ws:get(path), self()},
+                    %?debugVal("ws:close"),
+                    exit(self(), kill);
+            {send, Data} ->
+                    %?debugVal({"ws:send", Data}),
+                    Ws:send(Data),
+                    handle_websocket(Ws);
+            _Ignore ->
+                    handle_websocket(Ws)
+    after 5000 ->
+            %Ws:send(["pushing!"]),
+            handle_websocket(Ws)
+    end.
+
+handle_websocket(new, Ws) ->
+    %?debugVal({"handle_websocket:new:", Ws:get(path)}),
+    wsManager ! {add, Ws:get(path), self()},
+    handle_websocket(Ws).
+
+wsManager() ->
+    register(wsManager, self()),
+    process_flag(trap_exit, true),
+    wsManager([]).
+wsManager(ChannelList) ->
+    receive
+        {add, Channel, Pid} ->
+            %?debugVal({add, Channel, Pid}),
+            case lists:keysearch(Channel, 1, ChannelList) of
+                {value, {Channel, PidList}} ->
+                    case lists:member(Pid, PidList) of
+                        true ->
+                            wsManager(ChannelList);
+                        false ->
+                            NewPidList = lists:append([Pid], PidList),
+                            NewChannelList = lists:keyreplace(Channel, 1, ChannelList, {Channel, NewPidList}),
+                            %?debugVal(NewChannelList),
+                            wsManager(NewChannelList)
+                    end;
+                false ->
+                    wsManager(lists:append([{Channel, [Pid]}], ChannelList))
+            end;
+        {del, Channel, Pid} ->
+            %?debugVal({del, Channel, Pid}),
+            case lists:keysearch(Channel, 1, ChannelList) of
+                {value, {Channel, PidList}} ->
+                    case lists:member(Pid, PidList) of
+                        true ->
+                            NewPidList = lists:delete(Pid, PidList),
+                            NewChannelList = lists:keyreplace(Channel, 1, ChannelList, {Channel, NewPidList}),
+                            %?debugVal(NewChannelList),
+                            wsManager(NewChannelList);
+                        false ->
+                            wsManager(ChannelList)
+                    end;
+                false ->
+                    wsManager(ChannelList)
+            end;
+        {get, Channel, Pid} ->
+            %?debugVal({get, Channel, Pid}),
+            case lists:keysearch(Channel, 1, ChannelList) of
+                {value, {Channel, PidList}} ->
+                    Pid ! PidList;
+                false ->
+                    Pid ! false
+            end,
+            wsManager(ChannelList);
+        {send, Channel, Data} ->
+            case lists:keysearch(Channel, 1, ChannelList) of
+                {value, {_Channel, PidList}} ->
+                    %?debugVal(io:format("~p", [{Channel, PidList, Data}])),
+                    lists:map(fun(PID) -> PID ! {send, Data} end, PidList);
+                false ->
+                    pass
+            end,
+            wsManager(ChannelList);
+        {stat, Pid} ->
+            Pid ! {stat, ChannelList},
+            wsManager(ChannelList);
+        stop ->
+            exit(normal);
+        Any ->
+            ?debugVal(Any)
+    end.
